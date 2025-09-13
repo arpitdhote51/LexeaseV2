a8163e71c336f6d1994fb699fbc38bbd77f30588
Arpit Dhote <arpitdhote51@gmail.com>
2025-09-13 19:10:13 +0000

    genai key = AIzaSyDhXH5v9VknuSLZclH4NmQcL6ZZMNNTic0 Browser key (auto cr

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: "AIzaSyDcFJTJnGLI-uVStqI8uuQVcQMY34ilMJg"})],
+  plugins: [googleAI({apiKey: "AIzaSyDhXH5v9VknuSLZclH4NmQcL6ZZMNNTic0"})],
   enableTracingAndMetrics: true,
 });
