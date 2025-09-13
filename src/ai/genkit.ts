39700398ded6d5a6345dcc414a54859a42e27e94
Arpit Dhote <arpitdhote51@gmail.com>
2025-09-13 19:14:39 +0000

    now i have to reupload or change the secrets in secret manager also? in

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -2,6 +2,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: "AIzaSyDhXH5v9VknuSLZclH4NmQcL6ZZMNNTic0"})],
+  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
   enableTracingAndMetrics: true,
 });
