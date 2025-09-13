851d794a008d00bb5b0b88349c5cf2fa30dcc2c8
Arpit Dhote <arpitdhote51@gmail.com>
2025-09-13 19:08:27 +0000

    i have enabled all apis

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,7 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {firebase} from '@genkit-ai/firebase';
 
 export const ai = genkit({
   plugins: [googleAI({apiKey: "AIzaSyDcFJTJnGLI-uVStqI8uuQVcQMY34ilMJg"})],
-  model: 'googleai/gemini-2.5-flash',
   enableTracingAndMetrics: true,
 });
