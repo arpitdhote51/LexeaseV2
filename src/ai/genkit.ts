2589a15e4f0ca3c4d556503ca08c8c15e9311cda
Arpit Dhote <arpitdhote51@gmail.com>
2025-09-20 14:01:29 +0000

    see this error

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,9 +1,8 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {firebase} from '@genkit-ai/firebase';
 
 export const ai = genkit({
-  plugins: [googleAI(), firebase()],
+  plugins: [googleAI()],
   model: 'googleai/gemini-1.5-flash',
   enableTracingAndMetrics: true,
 });
