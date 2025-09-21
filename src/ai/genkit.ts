28d556ae7c7a1da6f53ee87dc287ad9bdbf25dcf
Arpit Dhote <arpitdhote51@gmail.com>
2025-09-21 08:20:33 +0000

    nothing is working the model is not analysing and lexy is not responding

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -3,6 +3,6 @@ import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
   plugins: [googleAI()],
-  model: 'googleai/gemini-1.5-flash',
+  model: 'googleai/gemini-1.5-pro-latest',
   enableTracingAndMetrics: true,
 });
