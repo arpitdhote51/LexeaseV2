17ef61e53176882b9af9356dacfa7ea8fadb75f8
Arpit Dhote <arpitdhote51@gmail.com>
2025-10-03 14:01:13 +0000

    change the model to latest one

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,13 +1,12 @@
 import {genkit} from 'genkit';
-import {googleAI} from '@genkit-ai/googleai';
+import {googleAI} from '@genkit-ai/google-genai';
 
 export const ai = genkit({
   plugins: [
     googleAI({
       apiKey: process.env.GEMINI_API_KEY,
-      apiVersion: 'v1',
     }),
   ],
-  model: 'googleai/gemini-1.0-pro',
+  model: 'googleai/gemini-1.5-pro-latest',
   enableTracingAndMetrics: true,
 });
