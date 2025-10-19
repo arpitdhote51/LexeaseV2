70ba46b7c8e71a014ea5b1b2b85c7b92d08d7763
Arpit Dhote <arpitdhote51@gmail.com>
2025-10-19 05:56:29 +0000

    Try fixing this error: `Unhandled Runtime Error: ChunkLoadError: Loading

--- src/app/new/page.tsx
+++ src/app/new/page.tsx
@@ -1,25 +1,7 @@
 "use client";
 
 import LexeaseLayout from "@/components/layout/lexease-layout";
-import { Skeleton } from "@/components/ui/skeleton";
-import dynamic from "next/dynamic";
-
-const LexeaseApp = dynamic(() => import('@/components/lexease-app'), {
-  ssr: false,
-  loading: () => <AnalysisPlaceholder />,
-});
-
-const AnalysisPlaceholder = () => (
-    <div className="p-10">
-        <div className="space-y-4 p-6">
-            <Skeleton className="h-8 w-1/3" />
-            <Skeleton className="h-40 w-full" />
-            <Skeleton className="h-8 w-1/4" />
-            <Skeleton className="h-20 w-full" />
-        </div>
-    </div>
-);
-
+import LexeaseApp from '@/components/lexease-app';
 
 export default function NewAnalysisPage() {
   return (
